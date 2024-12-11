e84103320c30eb68e3a5def7b18e347b9dd61ed7
Guillaume Fournier <me@guif.dev>
2024-12-11 13:59:28 +0100

    Add backup server to ssh config

--- ssh.nix
+++ ssh.nix
@@ -3,6 +3,11 @@
   services.ssh-agent.enable = true;
   services.gpg-agent.enable = true;
   programs.ssh = {
-    enable = false;
+    enable = true;
+    extraConfig = ''
+      Host backup.guif.dev
+        HostName backup.guif.dev
+        Port 30049
+    '';
   };
 }
