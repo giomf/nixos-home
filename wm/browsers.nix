ebab7f939c39ab93e30b35d0406d309acbc718cd
Guillaume Fournier <me@guif.dev>
2025-03-03 17:40:36 +0100

    Add librewolf

--- wm/browsers.nix
+++ wm/browsers.nix
@@ -1,9 +1,30 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     google-chrome
   ];
+
+  programs.librewolf = {
+    enable = true;
+    settings = {
+      "privacy.clearOnShutdown.history" = true;
+      "privacy.clearOnShutdown.cookies" = true;
+      "browser.privatebrowsing.autostart" = true;
+    };
+    profiles = {
+      guif = {
+        id = 0;
+        name = "guif";
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          darkreader
+          floccus
+          keepassxc-browser
+          ublock-origin
+        ];
+      };
+    };
+  };
 
   programs.firefox = {
     enable = true;
